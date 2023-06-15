5a22b09536c08c5e52288597d231dc79160000b4
MUHAMMAD AHMAD <ahmad.bsds@gmail.com>
2023-06-16 02:39:09 +0500

    Added explore option.

--- database.py
+++ database.py
@@ -12,7 +12,8 @@ class User(db.Model):  # The application context keeps track of the application-
     id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False, unique=True)
     name = db.Column(db.String(80), nullable=False)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    image = db.Column(db.LargeBinary, nullable= True)
+    # image = db.Column(db.LargeBinary, nullable=True)
+    recipe = db.Column(db.String(200), unique=True, nullable=False)
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -34,5 +35,5 @@ def print_data(table):
     """Function to show table record."""
     with app.app_context():
         data = table.query.all()  # Retrieve all records from the User table
-        for user in data:
-            return user.id, user.username
+        return data
+
--- server.py
+++ server.py
@@ -13,15 +13,23 @@ def home():
 # Add Recipe page
 @app.route('/addRecipe', methods=['GET', 'POST'])
 def add_recipe():
-    from database import User, insert_data, print_data
+    from database import User, insert_data
     if request.method == 'POST':
         name = request.form['name']
         username = request.form['username_']
-        # image = request.form['image']
-        insert_data(User, name=name, username=username)
-        data = print_data(User)
-        return f'{data}'
+        recipe = request.form['editordata']
+        insert_data(User, name=name, username=username, recipe=recipe)
+        return f'Action completed!'
     return render_template('addRecipe.html')
+
+
+@app.route('/explore', methods=['GET', 'POST'])
+def explore_recipies():
+    from database import User, print_data
+    if request.method == 'GET':
+        data = print_data(User)
+        return render_template('explore.html', data=data)
+    return 'Sorry!! Cant access.'
 
 
 if __name__ == "__main__":
